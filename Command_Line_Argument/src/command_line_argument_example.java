71b2881791b9321228d179348d52cf7c68626673
bhalla08 <46312211+bhalla08@users.noreply.github.com>
2021-05-03 15:26:53 +0530

    3RD MAY 3:26PM

---
+++ Command_Line_Argument/src/command_line_argument_example.java
@@ -0,0 +1,5 @@
+public class command_line_argument_example {
+    public static void main(String args[]){
+        System.out.println("Your first argument is: "+args[0]);
+    }
+}
---
+++ Command_Line_Argument/src/command_line_argument_example2.java
@@ -0,0 +1,8 @@
+public class command_line_argument_example2 {
+    public static void main(String args[]){
+
+        for(int i=0;i<args.length;i++)
+            System.out.println(args[i]);
+
+    }
+}
