79537e5c5e6548c55a8cf95e2427b0886e91396b
bhalla08 <46312211+bhalla08@users.noreply.github.com>
2021-05-28 13:54:44 +0530

    28/5

---
+++ COLLECTION_LINKEDLIST/src/LINKED_LIST_REVERSE_ELEMENTS.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LINKED_LIST_REVERSE_ELEMENTS {
+
+        public static void main(String args[]) {
+
+            LinkedList<String> ll = new LinkedList<String>();
+            ll.add("Ravi");
+            ll.add("Vijay");
+            ll.add("Ajay");
+            //Traversing the list of elements in reverse order
+            Iterator i = ll.descendingIterator();
+            while (i.hasNext()) {
+                System.out.println(i.next());
+            }
+
+        }
+    }
---
+++ COLLECTION_LIST_INTERFACE/src/LIST_TO_ARRAY.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LIST_TO_ARRAY {
+    public static void main(String args[]){
+        List<String> fruitList = new ArrayList<>();
+        fruitList.add("Mango");
+        fruitList.add("Banana");
+        fruitList.add("Apple");
+        fruitList.add("Strawberry");
+        //Converting ArrayList to Array
+        String[] array = fruitList.toArray(new String[fruitList.size()]);
+        System.out.println("Printing Array: "+ Arrays.toString(array));
+        System.out.println("Printing List: "+fruitList);
+    }
+}
